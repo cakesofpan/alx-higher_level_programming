c96cc1e3bb21dc146436133bbece6e96ec90d200
cakesofpan <gatibange@gmail.com>
2024-01-09 04:07:53 -0800

    Python Hiello 0.2

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery street")
+
+if type(number) == int:
+    print(f"{number} Battery street")
+
+else:
+    print("ValueError: Unknown format code 'd' for object of type 'str'")
