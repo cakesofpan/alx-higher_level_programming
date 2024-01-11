4fa5ca677a1016dfe23b73fb9f5593e96b4dfc0f
cakesofpan <gatibange@gmail.com>
2024-01-10 18:18:36 -0800

    Python Loops 0.2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(number)
 
 abs_num = abs(number)
 digit = abs_num % 10
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,6 @@ def print_last_digit(number):
     if (number < 0):
         number = -number
 
-    if (number == 0):
-        print("00")
     digit = number % 10
+    print(digit)
     return digit
