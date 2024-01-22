e9b798b24a0ef2d3c3e162e05e788f8ef448a0d2
cakesofpan <gatibange@gmail.com>
2024-01-22 15:34:09 -0800

    Python Data structures

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,9 @@
 def delete_at(my_list=[], idx=0):
     if not my_list:
         return ("[]")
-    
+
     if idx < 0 or idx >= len(my_list):
-        return(my_list)
+        return (my_list)
 
     my_list.remove(idx + 1)
-    return(my_list)
+    return (my_list)
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return (0, none)
+        return (0, None)
     else:
         first = sentence[0]
         length = len(sentence)
